d4fea6a4778b46ed4fd1971dc312ed7ee91b3824
castel50 <castel50@students.rowan.edu>
2016-06-02 17:11:35 -0400

    10

--- resources/views/editor/pass-map.blade.php
+++ resources/views/editor/pass-map.blade.php
@@ -20,49 +20,30 @@
 
 <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD3HYENbGDpPAJcVRRkJSkc9BYhf8HVVGI&callback=initMap"
   type="text/javascript"></script>
-
-  <div id="map"></div>
-
-<script>
-         var map;
-         function initMap() {
-  		// Create a map object and specify the DOM element for display.
-  		map = new google.maps.Map(document.getElementById('map'), {
-    	center: {lat: 39.696041, lng: -75.151347},
-   	 	scrollwheel: false,
-    	zoom: 8
-  		});
-	}
-         
-         //creates markers on the map
-         function createMarker(place) {
-           var placeLoc = place.geometry.location;
-             marker = new google.maps.Marker({
-             map: map2,
-             position: place.geometry.location
-           });
-         
-          //will display infowindow of given entertainment marker
-           google.maps.event.addListener(marker, 'mouseover', function() {
-         
-                  //set up our details request
-                 var detailsRequest = {placeId: place.place_id};
-         
-                 //preserve "this" to a variable so we can access it in the callback
-                 _this = this;
-                 service = new google.maps.places.PlacesService(map);
-                 service.getDetails(detailsRequest, detailsCallback, _this);
-           });
-         
-             google.maps.event.addListener(marker, 'mouseout', function() {
-             infowindow.close();
-           });
-         
-         }
-         
-         
-    //     google.maps.event.addDomListener(window, 'load', initialize);
-         
-             
-      </script>
+<title>Store locator with customisations</title>
+    <script
+      src="https://maps.googleapis.com/maps/api/js?libraries=places"></script>
+    <script
+      src="https://ajax.googleapis.com/ajax/libs/jquery/1.6/jquery.min.js">
+    </script>
+    <script src="infobubble-compiled.js"></script>
+    <script src="store-locator.min.js"></script>
+    <script src="medicare-static-ds.js"></script>
+    <script src="custom.js"></script>
+    <link rel="stylesheet" href="storelocator.css">
+    <style>
+      body { font-family: sans-serif; }
+      #map-canvas, #panel { height: 500px; }
+      #panel { width: 300px; float: left; margin-right: 10px; }
+      #panel .feature-filter label { width: 130px; }
+      p.attribution, p.attribution a { color: #666; }
+      .store .hours { color: grey; }
+    </style>
+  </head>
+  <body>
+    <h1>Medicare offices</h1>
+    <div id="panel"></div>
+    <div id="map-canvas"></div>
+    <p class="attribution">Medicare location data from <a href="http://data.gov.au/66">data.gov.au</a>, licensed under <a href="http://creativecommons.org/licenses/by/2.5/au/">CC-BY 2.5</a></p>
+  </body>
 @endsection

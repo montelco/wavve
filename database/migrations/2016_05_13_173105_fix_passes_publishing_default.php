d84feb2cfb6809b3943311160e4924862474a0db
Cory Monteleone <corymont@atmtllc.com>
2016-06-06 20:14:14 -0400

    Tired of getting an error message in deployment so I found the squeaky wheel.

--- database/migrations/2016_05_13_173105_fix_passes_publishing_default.php
+++ database/migrations/2016_05_13_173105_fix_passes_publishing_default.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class FixPassesPublishingDefault1 extends Migration
+class FixPassesPublishingDefault extends Migration
 {
     /**
      * Run the migrations.

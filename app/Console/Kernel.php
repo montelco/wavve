e966eb20adfcb8f061633d8bc8b036695391fd5c
Cory Monteleone <corymont@atmtllc.com>
2016-08-02 00:07:47 -0400

    Added in the scheduled service to remove demo passes from the account after five minutes.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+        $schedule->call(function () {
+            DB::table('passes')->where('user_id', '1')->delete();
+        })->everyFiveMinutes();
     }
 }

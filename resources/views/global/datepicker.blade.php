98161b143b631336011601d0839dc6873e137a7b
Cory Monteleone <corymont@atmtllc.com>
2016-09-21 19:26:14 -0400

    Other include files now load secure.

--- resources/views/global/datepicker.blade.php
+++ resources/views/global/datepicker.blade.php
@@ -1,4 +1,4 @@
-<link rel="stylesheet" href="http://code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
+<link rel="stylesheet" href="https://code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
 <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
 <script src="https://code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-ui-timepicker-addon/1.6.1/jquery-ui-timepicker-addon.min.js"></script>

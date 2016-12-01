bc27cd5c76103fa89a5d1bff1fc179e6cb890153
Cory Monteleone <corymont@atmtllc.com>
2016-11-30 22:27:48 -0500

    Fixed problem of not using User model on the schedule.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -6,6 +6,7 @@ use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 use Wavvve\Pass;
 use Carbon;
+use Wavvve\User;
 
 class Kernel extends ConsoleKernel
 {
@@ -30,18 +31,18 @@ class Kernel extends ConsoleKernel
             Pass::where('user_id', 1)->delete();
         })->everyFiveMinutes();
 
-        // $schedule->call(function () {
-        //     User::where('id', 1)->passes()->create([
-        //         'title' => "Free WiFi",
-        //         'template_number' => "1",
-        //         'primary_field' => "Free WiFi for all of our customers. Just check your receipt for the daily password.",
-        //         'secondary_field' => "Show this pass to the cashier for a special treat!",
-        //         'barcode_value' => rand(100001,999999),
-        //         'cashier_helper' => "Write down each use of this pass or enter reduction manually.",
-        //         'coupon_full_background_image' => "https://ucarecdn.com/990cc694-5f95-402a-8ff1-03b7a5fd5b08/",
-        //         'expiry' => Carbon\Carbon::now(),
-        //         'uuid' => str_random(7),
-        //     ]);
-        // })->everyFiveMinutes();
+        $schedule->call(function () {
+            User::where('id', 1)->passes()->create([
+                'title' => "Free WiFi",
+                'template_number' => "1",
+                'primary_field' => "Free WiFi for all of our customers. Just check your receipt for the daily password.",
+                'secondary_field' => "Show this pass to the cashier for a special treat!",
+                'barcode_value' => rand(100001,999999),
+                'cashier_helper' => "Write down each use of this pass or enter reduction manually.",
+                'coupon_full_background_image' => "https://ucarecdn.com/990cc694-5f95-402a-8ff1-03b7a5fd5b08/",
+                'expiry' => Carbon\Carbon::now(),
+                'uuid' => "RkW9oe6",
+            ]);
+        })->everyFiveMinutes();
     }
 }

29716c9098cd8372e9f75233684e56c21795fc7a
Cory Monteleone <corymont@atmtllc.com>
2016-12-20 13:24:33 -0500

    Fixing name of class in migration from a renaming.

--- database/migrations/2016_10_01_014516_create_visitors_table.php
+++ database/migrations/2016_10_01_014516_create_visitors_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class FinallyMakingCaseSensitiveUuid extends Migration
+class CreateVisitorsTable extends Migration
 {
     /**
      * Run the migrations.

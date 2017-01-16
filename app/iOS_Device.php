8013218302c485126b8b47c154aad3f8faf0a8a5
Cory Monteleone <corymont@atmtllc.com>
2017-01-15 22:21:11 -0500

    Add Mass Assignment override for remaining iOS Models

--- app/iOS_Device.php
+++ app/iOS_Device.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class iOS_Device extends Model
 {
     protected $table = 'ios_devices';
+
+    protected $fillable = [
+    	'device', 'push_token'
+    ];
 }
--- app/iOS_Registration.php
+++ app/iOS_Registration.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class iOS_Registration extends Model
 {
     protected $table = 'ios_registrations';
+
+    protected $fillable = [
+    	'uuid', 'pass_type_id', 'push_token', 'ios_devices_id', 'ios_passes_serial'
+    ];
 }

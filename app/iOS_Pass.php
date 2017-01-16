81c0ca98a1050a9c99a3ed95a8e0bc19955a3883
Cory Monteleone <corymont@atmtllc.com>
2017-01-15 20:36:44 -0500

    Resolves MassAssignment issue protection in Laravel

--- app/iOS_Pass.php
+++ app/iOS_Pass.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class iOS_Pass extends Model
 {
     protected $table = 'ios_passes';
+
+    protected $fillable = [
+    'passTypeID', 'authentication_token', 'serial_no'
+    ];
 }

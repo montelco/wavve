e0f7caf76728a86f5070ef03d89d7d64ddb38822
Cory Monteleone <corymont@atmtllc.com>
2017-01-21 21:09:29 -0500

    Changed dashboard for settings flash

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,6 +3,11 @@
 @include('editor.applets.latest-jquery')
 
 @section('dashContent')
+    @if (session('status'))
+    <div class="alert alert-success">
+        {{ session('status') }}
+    </div>
+    @endif
     <div class="row">
         <div class="col-lg-10 col-lg-offset-1">
             <h1 class="page-header">

d55dcecfcf461f0579152f5f72eb5f39bf82a7c8
Cory Monteleone <corymont@atmtllc.com>
2017-01-31 13:07:05 -0500

    Changed pass type

--- app/Http/Controllers/PassesController.php
+++ app/Http/Controllers/PassesController.php
@@ -190,7 +190,7 @@ class PassesController extends Controller
         define('ICON_FILE', '/home/forge/wavvve.io/public/ogfblg.jpg');
 
         // Create an event ticket
-        $pass = new StoreCard($results->username, $results->username);
+        $pass = new EventTicket($results->username, $results->name);
         $pass->setBackgroundColor('rgb(178, 215, 234)');
         //$pass->setLogoText('rgb(255,255,255)');
         $pass->setAuthenticationToken($results->apple_auth);
@@ -223,7 +223,7 @@ class PassesController extends Controller
         }
         
         // Add icon image
-        $icon = new Image(ICON_FILE, 'strip');
+        $icon = new Image(ICON_FILE, 'icon');
         $pass->addImage($icon);
 
         // Set pass structure

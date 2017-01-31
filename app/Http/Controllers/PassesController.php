3accdc2e7dffc2132cade93f3f6781ad42782e38
Cory Monteleone <corymont@atmtllc.com>
2017-01-31 13:03:01 -0500

    Background strip image

--- app/Http/Controllers/PassesController.php
+++ app/Http/Controllers/PassesController.php
@@ -223,7 +223,7 @@ class PassesController extends Controller
         }
         
         // Add icon image
-        $icon = new Image(ICON_FILE, 'background');
+        $icon = new Image(ICON_FILE, 'strip');
         $pass->addImage($icon);
 
         // Set pass structure

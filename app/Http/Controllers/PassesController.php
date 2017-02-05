1ea0440d72e0f0522f05fabb8014a81b25204cf9
Cory Monteleone-Haught <Cory Monteleone-Haught>
2017-02-04 21:40:03 -0500

    Trying again with Push.

--- app/Http/Controllers/PassesController.php
+++ app/Http/Controllers/PassesController.php
@@ -258,6 +258,46 @@ class PassesController extends Controller
             'serial_no' => $username,
         ]);
 
-        return PassFactory::serialize($pass);
+        PassFactory::serialize($pass);
+
+        $passphrase = '1234';
+        $deviceToken = '6429f7bade9ad66f2026e8ec6fed6a77c6d018475f1dbcc1728349565c7270bb';
+        $ctx = stream_context_create();
+        stream_context_set_option($ctx, 'ssl', 'local_cert', 'pushcert.pem');
+        stream_context_set_option($ctx, 'ssl', 'passphrase', $passphrase);
+
+        // Open a connection to the APNS server
+        $fp = stream_socket_client(
+          'ssl://gateway.push.apple.com:2195', $err,
+          $errstr, 60, STREAM_CLIENT_CONNECT | STREAM_CLIENT_PERSISTENT, $ctx);
+
+        if (! $fp) {
+            exit("Failed to connect: $err $errstr".PHP_EOL);
+        }
+
+        echo 'Connected to APNS'.PHP_EOL;
+
+        // Create the payload body
+        $body['aps'] = [];
+
+        // Encode the payload as JSON
+        $payload = json_encode($body);
+
+        // Build the binary notification
+        $msg = chr(0).pack('n', 32).pack('H*', $deviceToken).pack('n', strlen($payload)).$payload;
+
+        // Send it to the server
+        $result = fwrite($fp, $msg, strlen($msg));
+
+        if (! $result) {
+            echo 'Message not delivered'.PHP_EOL;
+        } else {
+            echo 'Message successfully delivered'.PHP_EOL;
+        }
+
+        // Close the connection to the server
+        fclose($fp);
+
+        return null;
     }
 }
